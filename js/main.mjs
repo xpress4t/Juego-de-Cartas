145818ccbe863cc3934924464245a57226a41cdb
Rafael Pérez García <real.lord.fido@gmail.com>
2024-07-18 23:56:36 +0200

    Add JS-Doc

--- js/main.mjs
+++ js/main.mjs
@@ -1,20 +1,52 @@
 import { cardSize } from './constants.mjs';
 
-// Players 2 - 10
-// Cards' constructor
+/**
+ * @typedef {Object} Coordinates
+ * @property {number} x - Card's X coordinate in background sprite
+ * @property {number} y - Card's Y coordinate in background sprite
+ */
+
+/**
+ *
+ * @param {string} s Card's symbol
+ * @param {string} c Card's color
+ * @param {number} p Card's score
+ * @param {number} x Card's X coordinate in background sprite
+ * @param {number} y Card's Y coordinate in background sprite
+ */
 function Card(s, c, p, x, y) {
   const symbol = s;
+  /**
+   * Returns the card's number or symbol
+   *
+   * @returns string
+   */
   this.getSymbol = () => symbol;
 
   const color = c;
+  /**
+   * Returns the card's color
+   *
+   * @returns string
+   */
   this.getColor = () => color;
 
   const score = p;
+  /**
+   * Return the card's score
+   *
+   * @returns string
+   */
   this.getScore = () => score;
 
   const picX = x;
   const picY = y;
 
+  /**
+   *
+   *
+   * @returns {Coordinates}
+   */
   this.getPicCoords = () => ({
     x: -(cardSize.width * picX - picX),
     y: -(cardSize.height * picY - picY / 2)
