ce271def979c1656d72cd098f0d18c1243122ed2
Emmanuel <emmanuelcm2899@gmail.com>
2024-07-27 10:22:30 +0200

    Added values to color and symbols

--- js/constants.mjs
+++ js/constants.mjs
@@ -1,4 +1,29 @@
 export const cardSize = {
   height: 110,
-  width: 74
+  width: 74,
+};
+
+export const color = {
+  blue: "blue",
+  green: "green",
+  red: "red",
+  yellow: "yellow",
+  black: "black",
+};
+
+export const symbols = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+  forbidden: "forbidden",
+  reverse: "reverse",
+  take2: "+2",
+  take4: "+4",
+  changeColor: "changeColor",
 };
