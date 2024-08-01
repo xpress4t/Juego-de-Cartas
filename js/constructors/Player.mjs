df6c9e95961a6c12e22d03af36a7623df8a47cae
Emmanuel <emmanuelcm2899@gmail.com>
2024-08-01 16:01:29 +0200

    I added the method to steal a card and its respective documentation

--- js/constructors/Player.mjs
+++ js/constructors/Player.mjs
@@ -1,26 +1,37 @@
 import { Card } from "./Card.mjs";
 
 /**
- *
  * @param {string} n Player's name
+ * @param {boolean} h Define whether the player is human or not
  */
-export function Player(n) {
+export function Player(n = "", h = false) {
   const name = n;
   /**
-   *
    * @returns {string}
    */
   this.getName = () => name;
   const score = 0;
   /**
-   *
    * @returns {number}
    */
   this.getScore = () => score;
   const cards = [];
   /**
-   *
    * @returns {Array.<Card>}
    */
   this.getCards = () => cards;
+  /**
+   * @param {Card} card Carta que el jugador está robando
+   */
+  this.drawCard = (card) => {
+    if (card) {
+      cards.push(card);
+    }
+  };
+
+  const human = h;
+  /**
+   * @returns {boolean}
+   */
+  this.isHuman = () => human;
 }
