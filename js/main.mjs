e6d97cf14c5ee547d34fda8af44dbaad1381a7bf
Emmanuel <emmanuelcm2899@gmail.com>
2024-08-20 10:40:06 +0200

    adding form

--- js/main.mjs
+++ js/main.mjs
@@ -8,12 +8,3 @@ const clara = new Player("Clarivel");
 
 const game = new Game([emma, clara], cards);
 game.init();
-
-// showCard(gameCards[0]);
-// game.getCards().forEach(showCard);
-emma.getCards().forEach(showCard);
-emma.isHuman();
-
-// Testing the nextPlayer and previousPlayer functions
-game.finishTurn();
-console.log(game.getActivePlayer().getName());
