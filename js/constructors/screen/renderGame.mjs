f326b6b7f7e8da4fc717877f531f5a967d5a3f38
xpress4t <emmanuelcm2899@gmail.com>
2024-09-18 10:51:55 +0200

    Adding cards on const htmlCode

--- js/constructors/screen/renderGame.mjs
+++ js/constructors/screen/renderGame.mjs
@@ -12,25 +12,45 @@ const htmlCode = `<div class="App GameScreen">
 
     <!-- ARRIBA -->
     <div class="Square PlayerCards PlayerCards--top">
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
     </div>
 
     <!-- DERECHA -->
     <div class="Square PlayerCards PlayerCards--right">
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
     </div>
 
     <!-- ABAJO -->
     <div class="Square PlayerCards PlayerCards--bottom">
-    <h3 class="PlayerName"></h3>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--horizontal"></div>
     </div>
 
     <!-- IZQUIERDA -->
     <div class="Square PlayerCards PlayerCards--left">
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
+      <div class="Square Card PlayerCards__Card PlayerCards__Card--vertical"></div>
     </div>
   </div>
 
   <div class="Sidebar">
     <div class="Square History">
       <h3>Historial</h3>
+      <p>Pepito jugó el 4 rojo</p>
+      <p>Fulanito jugó un "Saltar turno" rojo</p>
     </div>
 
     <div class="Square Time">Tiempo de juego</div>
